527546a2ecb574299f3b24f51740e861ade75a53
Kabir <sagithedeveloper@gmail.com>
2024-09-30 09:18:49 +0530

    added

---
+++ S21/src/app/components/rightimagecard/rightimagecard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rightimagecard',
+  standalone: true,
+  imports: [],
+  templateUrl: './rightimagecard.component.html',
+  styleUrl: './rightimagecard.component.css'
+})
+export class RightimagecardComponent {
+
+}
